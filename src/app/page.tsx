d879c3a1ea6e9635373b45c75fa5389209b9c899
icepik77 <74002871+icepik77@users.noreply.github.com>
2025-01-23 17:26:49 +0300

    add title and background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import NavBar from "./ui/nav";
 import { useState } from "react";
 import { montserrat } from "./fonts";
 
-const loveImage = "/love.png" 
+
+const loveImage = "/love-big.png" 
 
 export default function Home() {
 
@@ -15,29 +16,31 @@ export default function Home() {
   }
 
   return (
-    <>
-    <div className="flex flex-col items-center">
-      <NavBar cartItems = {cartItems}/>
-    </div>
-    <div className="flex flex-col items-center bg-[url('/public/section2-bg.png')]">
-      <div className={`${montserrat.className}`}>
-        <p className="pl-2">
-          Macaronshop
-        </p>
-        <div className="relative flex items-center">
-          <span className="absolute left-0 h-px w-5 bg-black content-['']"></span>
-          <p className="mx-6">since 2013</p>
-          <span className="absolute right-0 h-px w-5 bg-black content-['']"></span>
+    <div className="bg-[#EDEDF0] overflow-hidden">
+      <div className="flex flex-col items-center">
+        <NavBar cartItems = {cartItems}/>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className={`${montserrat.className}`}>
+          <p className="pl-2 mt-[65px] text-lg">
+            Macaronshop
+          </p>
+          <div className="relative flex items-center">
+            <span className="absolute left-0 h-px w-5 bg-black content-['']"></span>
+            <p className="mx-9 text-sm">since 2013</p>
+            <span className="absolute right-0 h-px w-5 bg-black content-['']"></span>
+          </div>
+        </div>
+        <div>
+          <h1 className={`${montserrat.className} text-center mt-[40px] text-2xl`}>Настоящая любовь</h1>
+          <p className="text-center max-w-[265px] text-sm mt-[5px]">
+            Пирожные макарон и другие десерты <br /> из натуральных ингредиентов, приготовленные с любовью
+          </p>
+        </div>
+        <div className="pl-[12%] scale-150 mt-[20px]">
+          <Image src={loveImage} alt="Настоящая любовь" width={313} height={260}/>
         </div>
       </div>
-      <div>
-        <h1 className={`${montserrat.className} text-center`}>Настоящая любовь</h1>
-        <p className="text-center max-w-[265px] text-sm">
-          Пирожные макарон и другие десерты <br /> из натуральных ингредиентов, приготовленные с любовью
-        </p>
-      </div>
-      <Image src={loveImage} alt="Насоящая любовь" width={313} height={260}/>
     </div>
-    </>
   );
 }
